5e1dbca1c6eac571eadb79159966a12f333e55a5
Muhammad Rizky Aulia Ramadhan <muhammadrizky.mrar@gmail.com>
2024-06-04 19:52:29 +0800

    perf: optimize sounds utility

---
+++ assets/js/sounds.js
@@ -0,0 +1,8 @@
+export const sounds = {
+    marin: new Audio('/assets/audio/marin.mp3'),
+    kobo: new Audio('/assets/audio/kobo.mp3'),
+    gura: new Audio('/assets/audio/gura.mp3'),
+    lily: new Audio('/assets/audio/lily.mp3'),
+    ninomae: new Audio('/assets/audio/ninomae.mp3'),
+    celebrate: new Audio('/assets/audio/sfx_celebrate_horn.mp3'),
+};
